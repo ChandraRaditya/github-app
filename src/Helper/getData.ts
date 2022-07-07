b39cea6db30b76603d28425ab6e8451dfe88a536
ChandraRaditya <chandraariwiraditya@gmail.com>
2022-07-08 00:04:00 +0700

    add: contentNumber

--- src/Helper/getData.ts
+++ src/Helper/getData.ts
@@ -20,7 +20,7 @@ export const getUserSearch = async (search: string) => {
     }
 }
 
-export const getReposSearch = async (search: string) => {
+export const getReposSearch = async (search: string, contentNumber: number) => {
     try {
         if(search.length > 0) {
             const options = {
@@ -28,7 +28,7 @@ export const getReposSearch = async (search: string) => {
                     'Authorization': `token ${token}`
                 }
             }
-            const res = await axios.get(`https://api.github.com/users/${search}/repos`,options)
+            const res = await axios.get(`https://api.github.com/users/${search}/repos?per_page=${contentNumber}`,options)
             const data: Repos[] = res.data
             return data;
         }
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -8,25 +8,44 @@ import "./index.css";
 
 function Home() {
   const [repo, setRepo] = useState<Repos[] | undefined>([]);
+  const [number, setNumber] = useState(5);
   const currentSearch: string = useSelector(
     (state: any) => state.serachData.value
   );
 
   useEffect(() => {
-    getReposSearch(currentSearch).then((response) => {
+    getReposSearch(currentSearch, number).then((response) => {
       console.log("data from api", response);
       setRepo(response);
     });
-  }, [currentSearch]);
+  }, [currentSearch, number]);
 
   const cardRepo = repo?.map((val) => {
     return <CardRepos id={val.id} name={val.name} language={val.language} />;
   });
 
+  const handleContentNumber = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    const { value } = event.target;
+    const data = parseInt(value);
+    setNumber(data);
+  };
+
   return (
     <div className="home-section">
       <div className="home-section-container">
         <Search />
+        <div>
+          <select
+            onChange={handleContentNumber}
+            name="content"
+            id="content-number"
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+          </select>
+        </div>
         <div className="card-repos-container">{cardRepo}</div>
       </div>
     </div>
